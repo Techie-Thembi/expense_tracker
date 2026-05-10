7d6d882f9d159ec86d0faec7de4a4f9844ff7984
Techie-Thembi <thembisilemakamu1@gmail.com>
2026-05-10 23:46:47 +0200

    Add initial project files and configuration for ExpenseManagement
    - Created ExpenseManagement.csproj.nuget.g.props and ExpenseManagement.csproj.nuget.g.targets for NuGet package management.
    - Added project.assets.json to manage project dependencies and assets.
    - Initialized project.nuget.cache for caching NuGet package information.
    - Created expense_tracker.sln solution file to organize the ExpenseManagement project.

---
+++ ExpenseManagement/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+//Transaction types and categories
+enum TransactionType
+{
+    Income,Expense
+}
+
+enum Category
+{
+    Salary,Freelance, Investment, Food, Transport, Enjoyment, Utilities, HealthCare, Shopping, Other
+}
+
+//Base clase for all transactions
+abstract class Transaction
+{
+    private static int _idCounter = 1;
+
+    public int Id { get; private set;}
+    public decimal Amount {get; set;}
+    public string Description {get; set;}
+    public DateTime Date {get; set;}
+
+    public Category Category {get; set;}
+    public TransactionType Type {get; protected set;}
+
+    protected Transaction(decimal amount, string descrption, Category category)
+    {
+        if (amount <= 0)
+        throw new ArgumentException("Amount must be positive");
+
+        Id = _idCounter++;
+        Amount = amount;
+        Description = descrption;
+        Category = category;
+        Date = DateTime.Today;
+    }
+
+    public abstract void Display();
+
+    public override string ToString()
+    {
+        return $"[ID: {Id}] {Date: dd/MM/yyyy} - {Category} - ${Amount:F2}";
+    }
+}
